a253c295005445431586a060f343d05c29e7b92a
CAM603 <cameronhawley@comcast.net>
2020-01-16 10:10:02 -0500

    added ref

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,12 +5,25 @@ import './Person.css';
 import Aux from '../../../HOC/Aux';
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+    componentDidMount() {
+        this.inputElementRef.current.focus();
+    }
+    
     render(){
         return (
             <Aux>
                 <p onClick={this.props.click}>I am a {this.props.name} and I am {this.props.age} years old! </p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+                <input 
+                type="text" 
+                onChange={this.props.changed} 
+                value={this.props.name}
+                ref={this.inputElementRef}
+                />
             </Aux>
         )
     }
